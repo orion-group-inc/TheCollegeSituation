052a785a9f5cef091ba9f8bc5c8bafd3bf980f9b
favour ori <orifavour2000@gmail.com>
2019-02-15 17:42:37 -0600

    updated api doc params

--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -1,1 +1,16 @@
-define({  "name": "College Situation",  "version": "0.1.0",  "description": "API documentation for college Situation",  "title": "Consumable API's",  "url": "http://localhost:5000/api",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-02-15T11:21:19.154Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
+define({
+  "name": "College Situation",
+  "version": "0.1.0",
+  "description": "API documentation for college Situation",
+  "title": "Consumable API's",
+  "url": "http://localhost:3000/api/v1/secure2019",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-02-15T23:41:01.693Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
