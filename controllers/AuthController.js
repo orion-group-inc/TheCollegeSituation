b83765cc3ae05b00a649fda9725d882aaeb06811
favour ori <orifavour2000@gmail.com>
2019-02-17 16:59:56 -0600

    checking for existing user before signing up studnt

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,75 +1,94 @@
-
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const Student = require("./../models/Student");
 
-class AuthController{
+class AuthController {
+  /**
+   * @api {post} /student/register Register Student
+   * @apiName Register Student
+   * @apiGroup Student
+   * @apiParam {String} firstName student's firstname
+   * @apiParam {String} lastName student's last name
+   * @apiParam {String} email student's email
+   * @apiParam {String} password student's password
+   * @apiParam {String} birthday student's birthday
+   */
+  static async registerStudent(req, res) {
+    //Create New Student (Work on the Mongo Db stuff here..)
+    var hashedPassword = bcrypt.hashSync(req.body.password, 8);
 
-    /**
-     * @api {post} /student/register Register Student
-     * @apiName Register Student
-     * @apiGroup Student
-     * @apiParam {String} firstName student's firstname
-     * @apiParam {String} lastName student's last name
-     * @apiParam {String} email student's email
-     * @apiParam {String} password student's password
-     * @apiParam {String} birthday student's birthday
-     */
-    static async registerStudent(req, res) {
-        //Create New Student (Work on the Mongo Db stuff here..)
-        var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+    let student = new Student({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      birthday: req.body.birthday,
+      password: hashedPassword
+    });
 
-        let student = new Student({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            birthday: req.body.birthday,
-            password: hashedPassword
-        });
+    Student.findOne({ email })
+      .then(user => {
+        if (email == user.email) {
+          res.status(400).send("A user with this email already exists");
+        } else {
+          //proceed to create account here
 
-        student
+          student
             .save()
             .then(newStudent => {
-            let token = jwt.sign({ id: newStudent._id, email: newStudent.email }, process.env.JWT_SECRET, {
-                expiresIn: 86400 // expires in 24 hours
-            });
-            res.status(200).send({
+              let token = jwt.sign(
+                { id: newStudent._id, email: newStudent.email },
+                process.env.JWT_SECRET,
+                {
+                  expiresIn: 86400 // expires in 24 hours
+                }
+              );
+              res.status(200).send({
                 success: true,
                 token,
                 newStudent
-            });
+              });
             })
             .catch(err => {
-            res.status(400).send("Could not create new student :(", err.message);
+              res
+                .status(400)
+                .send("Could not create new student :(", err.message);
             });
-    }
+        }
+      })
+      .catch(err => {
+        res.status(400).send("An error occured", err.message);
+      });
+  }
 
-    /**
-     * @api {post} /student/login Login Student
-     * @apiName Login Student
-     * @apiGroup Student
-     * @apiParam {String} email student's email
-     * @apiParam {String} password student's password
-     */
-    static async loginStudent(req, res) {
-        const {email, password} = req.body;
-            Student.findOne({email})
-                .then(user => {
-                    let isPasswordValid = bcrypt.compareSync(password, user.password);
-                    if(isPasswordValid){
-                        let token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, {
-                            expiresIn: 86400 // expires in 24 hours
-                        });
-                        res.send({auth: true, token, user });
-                    }else{
-                        res.status(401).send({auth: false, token: null});
-                    } 
-                })
-                .catch(err =>{
-                    res.send(err);
-                })
-    }
-  
+  /**
+   * @api {post} /student/login Login Student
+   * @apiName Login Student
+   * @apiGroup Student
+   * @apiParam {String} email student's email
+   * @apiParam {String} password student's password
+   */
+  static async loginStudent(req, res) {
+    const { email, password } = req.body;
+    Student.findOne({ email })
+      .then(user => {
+        let isPasswordValid = bcrypt.compareSync(password, user.password);
+        if (isPasswordValid) {
+          let token = jwt.sign(
+            { id: user._id, email: user.email },
+            process.env.JWT_SECRET,
+            {
+              expiresIn: 86400 // expires in 24 hours
+            }
+          );
+          res.send({ auth: true, token, user });
+        } else {
+          res.status(401).send({ auth: false, token: null });
+        }
+      })
+      .catch(err => {
+        res.send(err);
+      });
+  }
 }
 
 module.exports = AuthController;
