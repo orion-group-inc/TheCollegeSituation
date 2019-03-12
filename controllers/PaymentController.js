07c095e4ac25d4e6103aaf23bb23c6e1d199e14f
favour ori <orifavour2000@gmail.com>
2019-03-12 15:50:17 -0500

    merging

--- controllers/PaymentController.js
+++ controllers/PaymentController.js
@@ -5,10 +5,10 @@ const UserSubscription = require("./../models/UserSubscription");
 const Subscription = require("./../models/Subscription");
 const Student = require("./../models/Student");
 const Helper = require("./../helpers/helper");
-const axios = require('axios');
-const moment = require('moment');
+const axios = require("axios");
+const moment = require("moment");
 
-const {generateInvoice} = Helper;
+const { generateInvoice } = Helper;
 
 class PaymentController {
   /**
@@ -17,14 +17,20 @@ class PaymentController {
    * @apiGroup Payment
    */
   static async getAllPayments(req, res) {
-    Payment.find().populate('subscription').populate('authInfo', '-password').exec().then(allPayments => {
-      res.status(200).send({
-        success: true,
-        data: allPayments
+    Payment.find()
+      .populate("subscription")
+      .populate("authInfo", "-password")
+      .exec()
+      .then(allPayments => {
+        res.status(200).send({
+          success: true,
+          data: allPayments
+        });
+      })
+      .catch(err => {
+        console.log("Error finding invoice", err.message);
       });
-    });
   }
-
 
   /**
    * @api {post} /payment/createPayment Create a new Payment
@@ -44,23 +50,21 @@ class PaymentController {
     payment
       .save()
       .then(newPayment => {
-        Payment.findOne({_id: newPayment._id})
-              .populate('authInfo', '-password')
-              .populate('subscription')
-              .exec()
-              .then(refPayment => {
-                res.status(200).send({ success: true, data: refPayment });
-              })
-        
+        Payment.findOne({ _id: newPayment._id })
+          .populate("authInfo", "-password")
+          .populate("subscription")
+          .exec()
+          .then(refPayment => {
+            res.status(200).send({ success: true, data: refPayment });
+          });
       })
       .catch(err => {
         res.status(400).send("An error occoured", err.message);
       });
   }
 
-
   /**
-   * @api {get} /payment/getSinglePayment/:invoice 
+   * @api {get} /payment/getSinglePayment/:invoice
    * @apiName Get Single Payment
    * @apiGroup Payment
    * @apiParam {String} invoice invoice number of the payment
@@ -81,7 +85,7 @@ class PaymentController {
       });
   }
 
- /**
+  /**
    * @api {post} /payment/verifyPayment verify Payment
    * @apiName VerifyPayment
    * @apiGroup Payment
@@ -90,61 +94,73 @@ class PaymentController {
    */
   static async verifyPayment(req, res) {
     let key = await Key.findOne();
-    if(key){
-        let secret = key.live === false ? key.paystackTestSecretKey : key.paystackLiveSecretKey; 
+    if (key) {
+      let secret =
+        key.live === false
+          ? key.paystackTestSecretKey
+          : key.paystackLiveSecretKey;
 
-        try{
-          let invoice = await Payment.findOne({invoice: req.body.invoice});
-          if(invoice){
-            let response = await axios.get('https://api.paystack.co/transaction/verify/'+req.body.invoice,{
-              headers:{
-                Authorization: 'Bearer '+secret
-              }
-            })
-            
-            if(response.data.data.status === 'success'){
-              // checks if payment is verified
-              let userSubscription = await UserSubscription.findOne({authInfo: invoice.authInfo});
-              let student = await Student.findOne({_id: invoice.authInfo});
-              let subscription = await Subscription.findOne({_id: invoice.subscription});
-
-              if(userSubscription){
-                // adds days to end Date of subscription
-                userSubscription.endDate = moment(userSubscription.endDate).add(subscription.duration, 'days');
-                student.userSubscription = userSubscription;
-                await student.save();
-                let updatedUserSubscription = await userSubscription.save();
-                
-                res.send({message: 'Subscription successully updated', data: updatedUserSubscription});
-              }else{
-
-                // creates a new subscription for user
-                let newUserSubscription = new UserSubscription({
-                    authInfo: invoice.authInfo,
-                    subscription: invoice.subscription,
-                    endDate: moment().add(subscription.duration, 'days')
-                });
-                student.userSubscription = userSubscription;
-                await student.save();
-                let result = await newUserSubscription.save();
-                if(result){
-                  res.send({message: 'subscription successful', data: result})
-                }else{
-                  res.status(400).send({message: 'subscription failed'})
-                }
+      try {
+        let invoice = await Payment.findOne({ invoice: req.body.invoice });
+        if (invoice) {
+          let response = await axios.get(
+            "https://api.paystack.co/transaction/verify/" + req.body.invoice,
+            {
+              headers: {
+                Authorization: "Bearer " + secret
               }
             }
-          }else{
-            res.status(400).send({message: 'Couldnt find invoice'});
-          }
-        }catch(e){
-          res.status(400).send(e);
-        }
-        
-    }
-        
-  }
+          );
 
+          if (response.data.data.status === "success") {
+            // checks if payment is verified
+            let userSubscription = await UserSubscription.findOne({
+              authInfo: invoice.authInfo
+            });
+            let student = await Student.findOne({ _id: invoice.authInfo });
+            let subscription = await Subscription.findOne({
+              _id: invoice.subscription
+            });
+
+            if (userSubscription) {
+              // adds days to end Date of subscription
+              userSubscription.endDate = moment(userSubscription.endDate).add(
+                subscription.duration,
+                "days"
+              );
+              student.userSubscription = userSubscription;
+              await student.save();
+              let updatedUserSubscription = await userSubscription.save();
+
+              res.send({
+                message: "Subscription successully updated",
+                data: updatedUserSubscription
+              });
+            } else {
+              // creates a new subscription for user
+              let newUserSubscription = new UserSubscription({
+                authInfo: invoice.authInfo,
+                subscription: invoice.subscription,
+                endDate: moment().add(subscription.duration, "days")
+              });
+              student.userSubscription = userSubscription;
+              await student.save();
+              let result = await newUserSubscription.save();
+              if (result) {
+                res.send({ message: "subscription successful", data: result });
+              } else {
+                res.status(400).send({ message: "subscription failed" });
+              }
+            }
+          }
+        } else {
+          res.status(400).send({ message: "Couldnt find invoice" });
+        }
+      } catch (e) {
+        res.status(400).send(e);
+      }
+    }
+  }
 }
 
 module.exports = PaymentController;
