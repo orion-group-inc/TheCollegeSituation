3556e53ac6b9d1b25f292ba6284cde9b71b6aaae
franko4don <franko4don@yahoo.com>
2019-10-31 22:12:28 +0100

    added image upload via base 64 for scholarship

--- validations/ScholarshipValidator.js
+++ validations/ScholarshipValidator.js
@@ -1,8 +1,15 @@
 const Helpers = require('./../helpers/helper');
 const fs = require('fs');
+const path = require("path");
+const base = path.resolve(__dirname, '..');
 const {extractErrors} = Helpers;
 
-const savedDestination = 'scholarships/';
+const isBase64 = require('is-base64');
+
+let extension = 'jpg';
+
+const dest = "/public/scholarships/";
+
 
 /**
  * Defines methods for validating school Register functions
@@ -17,6 +24,7 @@ class ScholarshipValidator {
    * @param {callback} next
    */
   static validateScholarship(req, res, next) {
+    let photo = null;
     req.check('title', 'Title of house is required').notEmpty().trim();
     req.check('dueDate', 'dueDate is required').notEmpty().trim();
 
@@ -34,25 +42,36 @@ class ScholarshipValidator {
 
     req.check('status', 'status is required').notEmpty().trim();
     req.check('educationalLevel', 'educationLevel is required').notEmpty().trim();
-
-    req.checkBody('photo', 'Photo is required')
-        .custom((value) => {
-            if(req.file){
-                if(req.file.fieldname == 'photo'){
-                    return true;
-                }
-            }
+    req.check('photo', 'Scholarship Photo is required')
+    .custom((value) => {
+        if(!isBase64(value,{ mime: true })){
             return false;
-        }); 
-
-
+        }
+        let filename = Math.random().toString(36).substring(7)+ Date.now();
+        let status = false;
+        let base64Data = req.body.photo;
+        extension = '.'+base64Data.substring("data:image/".length, base64Data.indexOf(";base64"));
+        base64Data = base64Data.replace(/^data:image\/.*;base64,/,"");
+        fs.writeFileSync(base+dest+filename+extension, base64Data, 'base64', function(err) {
+            if (err) console.log(err);           
+        });
+        
+        try{
+            fs.readFileSync(base+dest+filename+extension);
+            photo = base+dest+filename+extension;
+            status = true;
+        }catch(err){
+            console.log(err.response);
+        }
+        return status;
+    });
+  
     const errors = req.validationErrors();
 
     if (errors) {
-        if(req.file){ 
-                       
-                fs.unlink(req.file.path, () => {
-                    console.log('deleted ' + req.file.path);
+        if(photo){
+            fs.unlink(photo, () => {
+                console.log('deleted ' + photo);
             });
         }
         
@@ -63,9 +82,8 @@ class ScholarshipValidator {
       });
     }
     
-    req.body.photo = savedDestination + req.file.filename;
+    req.body.photo = photo.replace(base+'/public/', '');
     return next();
   }
-
   }
   module.exports = ScholarshipValidator;
